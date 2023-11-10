e766faf0b55b1e89072cb3921114428deea09d91
aych <aychdev1@gmail.com>
2023-11-10 17:24:20 -0500

    updated code to use umi

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,42 @@
-import { AppProps } from 'next/app';
-import '../styles/globals.css';
-import Wallet from '@/component/WalletProvider';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import {
+  AlphaWalletAdapter,
+  LedgerWalletAdapter,
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+} from '@solana/wallet-adapter-wallets';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { useMemo } from 'react';
+import { UmiProvider } from '../utils/UmiProvider';
+import '@/styles/globals.css';
+import '@solana/wallet-adapter-react-ui/styles.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
+  if (!process.env.NEXT_PUBLIC_RPC_ENDPOINT) throw new Error('Missing RPC URL');
+
+  const endpoint = process.env.NEXT_PUBLIC_RPC_ENDPOINT;
+
+  const wallets = useMemo(
+    () => [
+      new SolflareWalletAdapter(),
+      new AlphaWalletAdapter(),
+      new LedgerWalletAdapter(),
+      new PhantomWalletAdapter(),
+    ],
+    []
+  );
   return (
-    <Wallet>
-      <Component {...pageProps} />
-    </Wallet>
+    <>
+      <WalletProvider wallets={wallets}>
+        <UmiProvider endpoint={endpoint}>
+          <WalletModalProvider>
+            <Component {...pageProps} />
+          </WalletModalProvider>
+        </UmiProvider>
+      </WalletProvider>
+    </>
   );
 }
-
-export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,58 +1,6 @@
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
-import { useConnection, useWallet } from '@solana/wallet-adapter-react';
-import { useEffect, useState } from 'react';
-import {
-  CandyMachine,
-  Metaplex,
-  Nft,
-  NftWithToken,
-  PublicKey,
-  Sft,
-  SftWithToken,
-  walletAdapterIdentity,
-} from '@metaplex-foundation/js';
-import { Keypair, Transaction } from '@solana/web3.js';
 
 export default function Home() {
-  const wallet = useWallet();
-  const { publicKey } = wallet;
-  const { connection } = useConnection();
-  const [metaplex, setMetaplex] = useState<Metaplex | null>(null);
-  const [candyMachine, setCandyMachine] = useState<CandyMachine | null>(null);
-  const [collection, setCollection] = useState<
-    Sft | SftWithToken | Nft | NftWithToken | null
-  >(null);
-  const [formMessage, setFormMessage] = useState<string | null>(null);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-
-  useEffect(() => {
-    (async () => {
-      if (wallet && connection && !collection && !candyMachine) {
-        if (!process.env.NEXT_PUBLIC_CANDY_MACHINE_ID) {
-          throw new Error('Please provide a candy machine id');
-        }
-        const metaplex = new Metaplex(connection).use(
-          walletAdapterIdentity(wallet)
-        );
-        setMetaplex(metaplex);
-
-        const candyMachine = await metaplex.candyMachines().findByAddress({
-          address: new PublicKey(process.env.NEXT_PUBLIC_CANDY_MACHINE_ID),
-        });
-
-        setCandyMachine(candyMachine);
-
-        const collection = await metaplex
-          .nfts()
-          .findByMint({ mintAddress: candyMachine.collectionMintAddress });
-
-        setCollection(collection);
-
-        console.log(collection);
-      }
-    })();
-  }, [wallet, collection, candyMachine]);
-
   return (
     <main>
       <div className='flex items-center justify-center h-screen'>

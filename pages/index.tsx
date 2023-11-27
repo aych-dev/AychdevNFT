41f1374bcccb1479d4f94eb8a18dae87233b315c
aych <aychdev1@gmail.com>
2023-11-27 18:15:43 -0500

    update css

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,8 +28,8 @@ export default function Home() {
   }, []);
 
   return (
-    <main className='flex items-center justify-center h-screen'>
-      <div className='bg-gray-300 rounded border shadow-sm flex-grow'>
+    <main className='flex items-center justify-center h-screen mx-11'>
+      <div className='bg-gray-300 rounded border shadow-xl min-w-full'>
         <div className='flex items-center justify-center'>
           <div className='mb-12 p-2'>
             <WalletMultiButtonDynamic />
